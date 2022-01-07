549f568806bda3891f597f341ef0a1b4d7b16471
xhyrom <generalkubo@gmail.com>
2022-01-07 21:44:27 +0100

    basbe

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
-import { Probot } from "probot";
+import { WebhookEvent, EventPayloads } from '@octokit/webhooks'
+import { Probot, Context } from 'probot';
+import command from './structures/Command';
 
-export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
+export default (app: Probot) => {
+  app.on('issues.opened', async (context) => {
+    const user = context.payload.sender.login;
+
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: [
+        `Hey @${user}! Thank you for opening the issue. However, we are not machines, so it may take us a while to get back to you.`,
+        ``,
+        `But if you don't want to report a problem, please contact our [discord server](https://discord.gg/AjKJSBbGm2)`
+      ].join('\n')
     });
+
     await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on(['issues.closed', 'issues.reopened'], async(context) => {
+
+    if (context.payload.action === 'closed') context.octokit.issues.lock(context.issue());
+    else context.octokit.issues.unlock(context.issue());
+  })
+
+  app.on('pull_request.opened', (context) => {
+    context.octokit.pulls.requestReviewers(context.pullRequest({ reviewers: ['xHyroM'] }));
+  })
+
+  //command(app, 'review', (context: WebhookEvent<EventPayloads.WebhookPayloadIssues> & Omit<Context<any>, keyof WebhookEvent<any>>, command: { arguments: string; }) => {
+    //const labels = command.arguments.split(/, */);
+
+    /*console.log(labels)
+    context.octokit.pulls.createReview(context.pullRequest({ event: 'APPROVE' }));
+    return context.octokit.issues.addLabels(context.issue({ labels }));
+  })*/
 };
